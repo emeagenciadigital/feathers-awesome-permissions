4c5c1ecb7c8db13befa20a8774fcaedcbc9cb11a
ezequiel bahoque <ezequielbahoquelopez@gmail.com>
2019-02-16 09:55:06 -0500

    add access fuctions in index file

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const {HasPermit} = require('./src/give-access-to');
-const Permissions = require('./src/permisions');
+const {configure: sequelize} = require('./sequelize');
+const {giveAccessTo} = require('./src/give-access-to');
+const {getUserRoles, getRequiredUserPermissions, getUserPermissions} = require('./src/permisions');
 
 module.exports = {
-	Permissions,
-	HasPermit
+	Permissions: {
+		getUserRoles, 
+		getRequiredUserPermissions, 
+		getUserPermissions
+	},
+	giveAccessTo,
+	Services: (type) => {
+		switch(type) {
+			case 'sequelize':
+				return sequelize;
+			default:
+				return sequelize;
+		}
+	}
 };
